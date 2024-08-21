5be783c12007eb5519ef18c8bb3e6e34a5df5e73
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-21 17:27:43 +0300

    Initial Commit

---
+++ App836/App836App.swift
@@ -0,0 +1,17 @@
+//
+//  App836App.swift
+//  App836
+//
+//  Created by IGOR on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct App836App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
